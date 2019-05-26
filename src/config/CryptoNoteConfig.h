0e58606e194a95ab7afb1924ba4aca1245fd7b06
Maayan <jjksvlogs@gmail.com>
2019-05-26 09:17:35 -0400

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -21,7 +21,7 @@ const uint64_t DIFFICULTY_TARGET                             = 25; // seconds
 const uint64_t DIFFICULTY_TARGET_V2                          = 25; // seconds
   
 /* Height to swap to DIFFICULTY_TARGET_V2 */
-const uint64_t DIFFICULTY_TARGET_V2_HEIGHT                   = 270000;
+const uint64_t DIFFICULTY_TARGET_V2_HEIGHT                   = 270000; // Date of fork is Sunday, June 23, 2019
 
 const uint32_t CRYPTONOTE_MAX_BLOCK_NUMBER                   = 500000000;
 const size_t   CRYPTONOTE_MAX_BLOCK_BLOB_SIZE                = 500000000;
@@ -44,7 +44,7 @@ static_assert(EMISSION_SPEED_FACTOR_V2 <= 8 * sizeof(uint64_t), "Bad EMISSION_SP
 
  
 /* Height to swap to EMISSION_SPEED_FACTOR_V2 */
-const uint64_t EMISSION_SPEED_FACTOR_V2_HEIGHT               = 270000;
+const uint64_t EMISSION_SPEED_FACTOR_V2_HEIGHT               = 270000; // Date of fork is Sunday, June 23, 2019
 
 
 /* Premine amount */
@@ -132,7 +132,7 @@ static_assert(UPGRADE_VOTING_WINDOW > 1, "Bad UPGRADE_VOTING_WINDOW");
 /* Block heights we are going to have hard forks at */
 const uint64_t FORK_HEIGHTS[] =
 {
-    270000
+    270000 // Date of fork is Sunday, June 23, 2019
 };
 
 /* MAKE SURE TO UPDATE THIS VALUE WITH EVERY MAJOR RELEASE BEFORE A FORK */
