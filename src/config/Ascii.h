c29ac42474631b277e3281f739fcca5d0a936283
sean12111 <sean12111@gmail.com>
2019-06-03 16:42:29 -0400

    Update Ascii.h

--- src/config/Ascii.h
+++ src/config/Ascii.h
@@ -1,6 +1,7 @@
 // Copyright (c) 2018, The TurtleCoin Developers
 // Copyright (c) 2018, The DeroGold Association
 // Copyright (c) 2019, The TurtleGold Developers
+// Copyright (c) 2019, The KalGold Developer
 // Please see the included LICENSE file for more information
 
 #pragma once
